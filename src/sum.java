2ec62427caa9d12b0b1e9f0b1e3ca9bcc0de7c46
Lakshmi Basapur <lakshmi.basapur@profinch.com>
2020-08-13 11:22:14 +0530

    yes

---
+++ src/sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("enter 2 numbers");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+    }
+}
